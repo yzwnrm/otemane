ca38cbfade6736ae5a4b145db1d1c8db0b93f128
yzwnrm <dkdmksgrsgtsr4@gmail.com>
2025-03-21 04:52:33 +0900

    ログイン、ユーザー登録・html作成　エラー発生中

--- otemane/app/urls.py
+++ otemane/app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import (HomeView)
+from .views import (
+    RegistUserView, HomeView, UserLoginView,
+    UserLogoutView
+)
 
 app_name = 'app'
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
+    path('regist/', RegistUserView.as_view(), name='regist'),
+    path('user_login/', UserLoginView.as_view(), name='user_login'),
+    path('user_logout/', UserLogoutView.as_view(), name='user_logout'),
 ]
--- otemane/app/views.py
+++ otemane/app/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import(
+    TemplateView, CreateView, FormView, View
+)
+from django.urls import reverse_lazy
+from .forms import RegistForm
 
-class HomeView(View):
-    def get(self, request):
-        return render(request,
-                      'home.html')
+class HomeView(TemplateView):
+    template_name = 'home.html'
     
-class RegistUserView(View):
-    def get(self, request):
-        return render(request,
-                      'regist.html')
-    # Create your views here.
+class RegistUserView(CreateView):
+    template_name = 'regist.html'
+    form_class = RegistForm
+    success_url = reverse_lazy('app:home')
+    
+class UserLoginView(View):
+    template_name = 'user_login.html'
+
+class UserLogoutView(View):
+    pass
