e559216744c96dd208504c41f9298f040b091fed
yzwnrm <dkdmksgrsgtsr4@gmail.com>
2025-05-17 21:40:03 +0900

    topページ修正

--- otemane/otemane/urls.py
+++ otemane/otemane/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('otemane/', include('app.urls')),
+    path('', views.portfolio, name='portfolio'),
+
 ]
