0f7db185edfaca3dc88f614ba372d915790a4591
yzwnrm <dkdmksgrsgtsr4@gmail.com>
2025-05-18 16:53:43 +0900

    カスタムユーザー準備

--- otemane/app/admin.py
+++ otemane/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Helps, Rewards
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Helps, Rewards, User
 
 
 class RewardInline(admin.StackedInline): 
@@ -8,5 +9,26 @@ class RewardInline(admin.StackedInline):
 
 class HelpsAdmin(admin.ModelAdmin):
     inlines = [RewardInline]
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('user_name', 'family', 'relationship')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_at', 'update_at')}),
+        ('Password reset', {'fields': ('password_token', 'password_expiry')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'family', 'relationship', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'user_name', 'family', 'relationship', 'is_staff', 'is_active')
+    search_fields = ('email', 'user_name', 'family__name')
+    ordering = ('email',)
+
 
 admin.site.register(Helps, HelpsAdmin)  # 管理画面に登録
