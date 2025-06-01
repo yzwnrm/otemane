dd66e41e8ba690b02c5e7a5c8654cf0cfb2b602c
yzwnrm <dkdmksgrsgtsr4@gmail.com>
2025-06-01 14:05:09 +0900

    パスワードリセットメール設定

--- otemane/otemane/settings.py
+++ otemane/otemane/settings.py
@@ -134,4 +134,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/otemane/user_login/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'otemane.mail@gmail.com'
+EMAIL_HOST_PASSWORD = 'vbmh eimj muva ryeq' 
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
